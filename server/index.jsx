905c689d24460cc41449d972a108969196346e4d
Dániel Agg <daniel.agg@outlook.com>
2019-05-19 16:51:45 +0200

    Added get question and questions endpoints w/ to return mock data

--- server/index.jsx
+++ server/index.jsx
@@ -2,9 +2,40 @@ import express from "express";
 import yields from "express-yields";
 import fs from "fs-extra";
 import webpack from "webpack";
+import { argv } from "optimist";
+import { get } from "request-promise";
+import { delay } from "redux-saga";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+function* getQuestions() {
+  let data = yield fs.readFile("./mock-data/data.json", "utf-8");
+  return JSON.parse(data);
+}
+
+function* getQuestion(question_id) {
+  const questions = yield getQuestions();
+  const question = questions.items.find(
+    _question => _question.question_id == question_id
+  );
+  question.body = `Mock question body: ${question_id}`;
+  let data = { items: [question] };
+
+  return data;
+}
+
+app.get("/api/questions", function*(req, res) {
+  const data = yield getQuestions();
+  yield delay(150);
+  res.json(data);
+});
+
+app.get("/api/questions/:id", function*(req, res) {
+  const data = yield getQuestion(req.params.id);
+  yield delay(150);
+  res.json(data);
+});
 
 if (process.env.NODE_ENV === "development") {
   const config = require("../webpack.config.dev.babel").default;
